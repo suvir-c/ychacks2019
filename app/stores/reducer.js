daa9cf29cfb5242a550aede4445419ce51b3c825
suvir-c <scoppara@usc.edu>
2019-04-13 10:06:01 -0700

    Add form to contact representatives page + relevant redux state updates

--- app/stores/reducer.js
+++ app/stores/reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import UserReducer from './user/reducer';
+import FormReducer from './form/reducer';
 
 export default combineReducers({
   user: UserReducer,
+  forms: FormReducer,
 });
